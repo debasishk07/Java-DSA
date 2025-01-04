6a2da8fc8d6dd73be6377330d09f8606f70b550e
Debasish Mohanta <debasishk7809@gmail.com>
2025-01-04 22:45:00 +0530

    added different sorting algorithms like Insertion Sort, Inbuilt sort, Counting sort

---
+++ Sorting Algorithms/InbuiltSort.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class InbuiltSort {
+    public static void main(String args[]){
+        int arr[] ={8,4,5,6,3};
+        Arrays.sort(arr);
+
+        for(int i =0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }    
+}
